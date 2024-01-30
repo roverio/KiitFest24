6c7e8c010535685223bfba1f7b5720fbd2eef86b
Revenant <dasdev.pratik@gmail.com>
2024-01-30 09:28:20 +0530

    billdesk response route added

--- src/app/api/paymentVerify/route.js
+++ src/app/api/paymentVerify/route.js
@@ -2,20 +2,52 @@ import CryptoJS from "crypto-js";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
+  const searchParams = request.nextUrl.searchParams;
+  const code = searchParams.get("msg").toString();
+  let values = code.split("|");	
+  let checksumUrl = values[values.length - 1];
 
-const url = "MERCHANTID|ARP10234|MSBI0412001668|NA|00000094.00|SBI|22270726|NA|INR|NA|NA|NA|NA|12-12-2004 16:08:56|0300|NA|NA|NA|NA|NA|NA|NA|NA|NA|NA|3734835005"
+  // Example usage
+  const receivedMsg = code; // Replace with the actual received message
+  const receivedChecksum = checksumUrl; // Replace with the actual received checksum
+  const checksum_key = process.env.BILLDESK_CHECKSUM;
 
-const params = new URLSearchParams(request.url)
-const messageToDecode = params.get("msg")
-console.log(params.get("msg"));
-console.log(messageToDecode);
+  // Validate the checksum
+  function validateChecksum(receivedMsg, receivedChecksum, key) {
+    const hmac = CryptoJS.HmacSHA256(receivedMsg, key).toString().toUpperCase();
+	
+    const calculatedChecksum = CryptoJS.enc.Hex.stringify(hmac);
 
-return NextResponse.json(
-	{
-	  message: "Pay successfull",
-	},
-	{
-	  status: 200,
-	}
+    const calculatedChecksumUpperCase = calculatedChecksum.toString().toUpperCase();
+	console.log(hmac, 'calculatedChecksumUpperCase');
+	console.log(receivedChecksum, 'receivedchecksum');
+	console.log(receivedMsg, 'receivedMsg');
+
+
+    if (calculatedChecksumUpperCase === receivedChecksum) {
+		const kfId = values[1];
+		// write code to update the database with the payment status
+      console.log("Checksum validation successful");
+    } else {
+      console.error("Checksum validation failed");
+    }
+  }
+
+  validateChecksum(receivedMsg, receivedChecksum, checksum_key);
+
+
+  return NextResponse.json(
+    {
+      message: "Pay successfull",
+    },
+    {
+      status: 200,
+    }
   );
 }
+
+// const newValues = values.slice(0, values.length - 1).join("|");
+// console.log(newValues, 'newValues');
+// const calculatedChecksum = CryptoJS.HmacSHA256(newValues, `${checksumUrl}`)
+// .toString()
+// .toUpperCase();
