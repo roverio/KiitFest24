0f70870558efe5762cdba6d9bccfee31bf27adaa
Revenant <dasdev.pratik@gmail.com>
2024-01-30 17:50:53 +0530

    update payment client

--- src/app/api/paymentVerify/route.js
+++ src/app/api/paymentVerify/route.js
@@ -1,53 +1,52 @@
 import CryptoJS from "crypto-js";
 import { NextResponse } from "next/server";
+import { db } from "@/server/db";
+import { redirect } from "next/navigation";
+
+// Function to calculate HMAC-SHA256 hash using CryptoJS
+function calculateHmacSha256(str, key) {
+  const hmac = CryptoJS.HmacSHA256(str, key);
+  return hmac.toString().toUpperCase();
+}
 
 export async function POST(request) {
-  const searchParams = request.nextUrl.searchParams;
-  const receivedMsg = searchParams.get("msg").toString();
-  let values = receivedMsg.split("|");
-  let lastCheckSumValue = values[values.length - 1];
+  const data = await request.formData();
+  console.log(data);
+  let receivedMsg = data.get("msg");
+  let splitData = receivedMsg.split("|");
+  let lastCheckSumValue = splitData[splitData.length - 1];
 
-  // Example usage
   const checksum_key = process.env.BILLDESK_CHECKSUM;
 
   if (receivedMsg !== "") {
     const stringNew = receivedMsg.replace("|" + lastCheckSumValue, ""); // Replace "|" and the code with an empty string
-
-	console.log(stringNew, 'stringNew')
+    console.log(stringNew, "stringNew");
     const calculatedChecksum = calculateHmacSha256(
       stringNew,
       `${checksum_key}`
     );
 
-    console.log(calculatedChecksum, lastCheckSumValue);
+    const customer1 = splitData[1];
+    console.log(customer1);
+
     if (calculatedChecksum === lastCheckSumValue && splitData[14] === "0300") {
       const customer = splitData[1];
       const txn = splitData[2];
       const amt = splitData[4];
-
-	  console.log(customer, txn, amt);
-	//   run db query here to update payment status
-	
+      console.log(customer, txn, amt);
+      //   run db query here to update payment status
+      await db.user.update({
+        where: {
+          kfid: customer,
+        },
+        data: {
+          isPaymentCompleted: true,
+        },
+      });
+      // write a newquery to log payments if possible
+      return redirect("/paymentnotsuccess");
     }
-
-    // Function to calculate HMAC-SHA256 hash using CryptoJS
-    function calculateHmacSha256(str, key) {
-      const hmac = CryptoJS.HmacSHA256(str, key);
-      return hmac.toString().toUpperCase();
-    }
-
-    return NextResponse.json(
-      {
-        message: "Pay successfull",
-      },
-      {
-        status: 200,
-      }
-    );
+    return redirect("/paymentnotsuccess");
   }
+  return redirect("/paymentnotsuccess");
 }
-// const newValues = values.slice(0, values.length - 1).join("|");
-// console.log(newValues, 'newValues');
-// const calculatedChecksum = CryptoJS.HmacSHA256(newValues, `${lastCheckSumValue}`)
-// .toString()
-// .toUpperCase();
