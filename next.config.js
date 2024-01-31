9b9d8342a62b1a0412ec6ef23dbabca9a715063e
Revenant <dasdev.pratik@gmail.com>
2024-01-31 21:00:03 +0530

    making it live => 1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,35 @@ const nextConfig = {
     images: {
         domains: ["res.cloudinary.com", "picsum.photos"],
     },
+    async redirects() {
+        return [
+          {
+            source: '/events',
+            destination: '/coming-soon',
+            permanent: true,
+          },
+          {
+            source: '/events/:slug',
+            destination: '/coming-soon',
+            permanent: true,
+          },
+          {
+            source: '/members',
+            destination: '/coming-soon',
+            permanent: true,
+          },
+          {
+            source: '/auth/signup',
+            destination: '/coming-soon',
+            permanent: true,
+          },
+          {
+            source: '/auth/login',
+            destination: '/coming-soon',
+            permanent: true,
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
