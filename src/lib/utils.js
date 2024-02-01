04573b0e08c418727b38f4ddf9c44426021fcb29
Revenant <dasdev.pratik@gmail.com>
2024-02-01 06:57:33 +0530

    production mail link test

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -26,7 +26,7 @@ export const sendConfirmationEmail = async ({
       <p style="text-align: center; color: #1a1a1a;">Hi ${name},</p>
       <p style="text-align: center; color: #1a1a1a;">Thanks for registering for KIIT Fest 2024. Please verify your email by clicking the button below.</p>
       <div style="text-align: center;">
-        <a href="http://localhost:3000/api/auth/confirm?code=${verificationCode}" style="background-color: #1a1a1a; color: #fff; padding: 10px 20px; border-radius: 5px; text-decoration: none;">Verify Email</a>
+        <a href="http://kiitfest.org/api/auth/confirm?code=${verificationCode}" style="background-color: #1a1a1a; color: #fff; padding: 10px 20px; border-radius: 5px; text-decoration: none;">Verify Email</a>
       </div>
       <p style="text-align: center; color: #1a1a1a;">Regards,<br />KIIT FEST</p>
     `,
