be5ee243402ad34bacf8fcd5fc64a0961251e276
Revenant <dasdev.pratik@gmail.com>
2024-02-06 01:11:10 +0530

    adding technical part 1

--- src/app/events/[eventId]/page.jsx
+++ src/app/events/[eventId]/page.jsx
@@ -17,7 +17,6 @@ const EventPage = async ({ params }) => {
   });
 
   const session = await getServerAuthSession();
-  // if(session)ankitkumar19041@gmail.com
 
   if (session) {
     const userId = session.user.id || null;
@@ -42,11 +41,23 @@ const EventPage = async ({ params }) => {
           <Card event={events} showRegister={false} />
           <section className="w-[90vw] mx-auto lg:w-[60vw] lg:h-[85vh] flex flex-col justify-between items-center text-white bg-gradient-to-r from-[#ffffff1a] to-[#ffffff00] backdrop-blur-2xl border-[#130C5C] rounded-xl border-[1px] md:py-[60px] md:px-12 px-6 text-lg py-8 gap-8 md:text-xl">
             {/* <IoCaretBack className="text-white text-xl"/> */}
-            <p>{events.description}</p>
-            <div>
-              <p>Partcipation: {events.memberType}</p>
-              <p>Category: {events.category}</p>
-            </div>
+            {events.type === "CULTURAL" ? (
+              <div>
+                <p>{events.description}</p>
+                <div>
+                  <p>Partcipation: {events.memberType}</p>
+                  <p>Category: {events.category}</p>
+                </div>
+              </div>
+            ) : (
+              <div className="flex"> <p className="mr-2">Check Guidelines: </p> 
+                <a target="_blank" href={events.description}>
+                  <button className="bg-blue-700 px-4 py-2 rounded-md uppercase text-sm tracking-wider hover:bg-blue-600 transition-colors duration-200">
+                    Guidelines
+                  </button>
+                </a>
+              </div>
+            )}
             <RegisterButton
               isRegistered={registeredEventData ? true : false}
               eventId={eventId}
@@ -69,18 +80,29 @@ const EventPage = async ({ params }) => {
             <Card event={events} showRegister={false} />
             <Link href="/events" className="max-w-72 flex-shrink-0">
               <button className="flex items-center text-white self-center mx-auto font-semibold font-sans mt-4 uppercase justify-center w-full bg-blue-800 rounded-md py-2 gap-1 group max-w-72 hover:bg-blue-700 transition-colors duration-300">
-                <IoCaretBack className="text-white text-2xl top-4 lef-6 group-hover:-translate-x-2 transition-all duration-300"/>
+                <IoCaretBack className="text-white text-2xl top-4 lef-6 group-hover:-translate-x-2 transition-all duration-300" />
                 Back to events
               </button>
             </Link>
           </div>
           <section className="w-[90vw] mx-auto lg:w-[60vw] lg:h-[85vh] flex flex-col  text-white bg-gradient-to-r from-[#ffffff1a] to-[#ffffff00] backdrop-blur-2xl border-[#130C5C] rounded-xl border-[1px] md:py-[60px] md:px-12 px-6 text-lg py-8 gap-8 md:text-xl">
-            <p>{events.description}</p>
-            <div>
-              <p>Partcipation: {events.memberType}</p>
-              <p>Category: {events.category}</p>
-            </div>
-
+          {events.type === "CULTURAL" ? (
+              <div>
+                <p>{events.description}</p>
+                <div>
+                  <p>Partcipation: {events.memberType}</p>
+                  <p>Category: {events.category}</p>
+                </div>
+              </div>
+            ) : (
+              <div className="flex"> <p className="mr-2">Check Guidelines: </p> 
+                <a target="_blank" href={events.description}>
+                  <button className="bg-blue-700 px-4 py-2 rounded-md uppercase text-sm tracking-wider hover:bg-blue-600 transition-colors duration-200">
+                    Guidelines
+                  </button>
+                </a>
+              </div>
+            )}
             <Link href="/auth/login">
               <button className="text-sm lg:text-[17px] font-semibold bg-gradient-to-b from-[#174ACE] rounded-full border-white border-[2px] to-[#16B2DB] px-4 py-1  lg:px-9 lg:py-3">
                 Login to Register
