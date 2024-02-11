3deb47c6841312f331b532f129f6b01262ffdcec
Revenant <dasdev.pratik@gmail.com>
2024-02-12 00:07:53 +0530

    finally removing registration

--- prisma/seed/index.js
+++ prisma/seed/index.js
@@ -7,22 +7,22 @@ const prisma = new PrismaClient();
 // randomly throws cultural or technical
 
   async function seed() {
-    const user = await prisma.user.findMany({
-      where: {
-        isPaymentCompleted: false,
-      },
-      select: {
-        email: true,
-        name: true,
-        phoneNumber: true,
-        isKiitStudent: true,
-        institution: true,
-      },
-    });
-    console.log(user);
-    const jsonData = JSON.stringify(user);
-    console.log(jsonData);
-    fs.writeFileSync('output.json', jsonData);
+    // const user = await prisma.user.findMany({
+    //   where: {
+    //     isPaymentCompleted: false,
+    //   },
+    //   select: {
+    //     email: true,
+    //     name: true,
+    //     phoneNumber: true,
+    //     isKiitStudent: true,
+    //     institution: true,
+    //   },
+    // });
+    // console.log(user);
+    // const jsonData = JSON.stringify(user);
+    // console.log(jsonData);
+    // fs.writeFileSync('output.json', jsonData);
   }
 
 
